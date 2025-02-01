982895e23d463907e102c96b9deceaaa1386428b
dannyp12710 <danielpinilla280@gmail.com>
2025-01-31 21:01:53 -0500

    Create test_assignment_1.py

---
+++ test/test_assignment_1.py
@@ -0,0 +1,24 @@
+# For the problem with Approximation Algorithm 
+# x0 = 1.5
+# tol = 0.000001
+# iter = 0
+# diff = x0
+# x = x0
+
+# For the problem with The Bisection Method 
+# def f(x):
+# return x**3 + 4*x**2 - 10
+# a = 1.0
+# b = 2.0
+# tol = 0.001  # Accuracy 10^-3
+
+
+# For the problem with The fixed-point Iteration 
+# i_p0 = 1.5
+# p = p0 - p0**3 - 4 * p0**2 + 10
+
+# For the problem with the Newton-Raphson method
+# def f(x): return math.cos(x) - x
+# def f_prime(x): return -math.sin(x) - 1
+# initial_approximation = 0.5 
+# The interval was from [0 , pi/2]
